d69c3a7a6a3f1d78497b772231859d167d330aa1
S-117 <110575016+juandlopez2186@users.noreply.github.com>
2023-11-07 23:16:51 -0500

    Add files via upload

---
+++ proyecto9/script.js
@@ -0,0 +1,81 @@
+let eventos=[];
+let arr=[];
+
+const nombreEvento=document.querySelector("#nombreEvento")
+const fechaEvento=document.querySelector("#fechaEvento")
+const agregar=document.querySelector("#agregar")
+const listaEventos=document.querySelector("#listaEventos")
+const json=cargar()
+try {
+    arr=JSON.parse(json)
+} catch (error) {
+    arr=[]
+}
+eventos= arr?[...arr]:[];
+mostrarEventos()
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    agregarEvento();
+})
+function agregarEvento() {
+    if (nombreEvento.value === "" || fechaEvento.value === "") {
+        return
+    }
+
+    if (diferencia(fechaEvento.value)<0) {
+        return
+    }
+
+    const nuevoEvento={
+        id:(Math.random() * 100).toString(36).slice(3),
+        nombre: nombreEvento.value,
+        fecha: fechaEvento.value
+    }
+
+    eventos.unshift(nuevoEvento)
+    guardar(JSON.stringify(eventos))
+    nombreEvento.value=""
+    fechaEvento.value=""
+    mostrarEventos()
+    console.log(mostrarEventos())
+}
+function diferencia(destino) {
+    let fechaDestino = new Date(destino);
+    let fechaActual = new Date();
+    let diferenciaF= fechaDestino.getTime() - fechaActual.getTime()
+    let dias = Math.ceil(diferenciaF / (1000 * 3600 * 24))//milisegundos => horas => Dias
+    return dias
+}
+function mostrarEventos() {
+    const eventosHTML=eventos.map((evento)=>{
+        return`
+        <div class="evento">
+            <div class="dias">
+                <span class="diasfaltantes">${diferencia(evento.fecha)}</span><br>
+                <span class="texto">dias<br>para</span>
+            </div>
+            <div class="nombreEvento">${evento.nombre}</div>
+            <div class="fechaEvento">${evento.fecha}</div>
+            <div class="acciones">
+                <button data-id="${evento.id}" class="eliminar">Eliminar</button>
+                </div>
+        </div>`
+    })
+    listaEventos.innerHTML=eventosHTML.join("");
+    document.querySelectorAll('.eliminar').forEach(button => {
+        button.addEventListener("click", e => {
+            const id = button.getAttribute('data-id');
+            eventos = eventos.filter(evento=> evento.id!= id);
+            guardar(JSON.stringify(eventos))
+            mostrarEventos()
+        })
+    });
+}
+function guardar(datos) {
+    localStorage.setItem("lista",datos)
+    
+}
+function cargar(datos) {
+    return localStorage.getItem("lista")
+}
